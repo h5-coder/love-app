6bdd2818c900bde30ef50775611ff793d5869670
lovely-man <liangyanxiangde@163.com>
2019-11-20 20:59:52 +0800

    init

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import RootNavigator from './MainNavigator';
+
+class App extends React.Component {
+    render() {
+        return <RootNavigator />
+    }
+}
+
+export default App;
