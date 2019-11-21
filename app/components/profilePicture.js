995c43886dec7f0033f2e4757cfce7c0028c6b4b
lovely-man <liangyanxiangde@163.com>
2019-11-21 19:00:31 +0800

    update

--- app/components/profilePicture.js
+++ app/components/profilePicture.js
@@ -8,7 +8,7 @@ console.log(boy, girl);
 
 const styles = StyleSheet.create({
     list: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
         paddingVertical: 5,
@@ -16,6 +16,10 @@ const styles = StyleSheet.create({
         borderBottomWidth: StyleSheet.hairlineWidth,
         borderColor: '#d6d7da',
     },
+    main: {
+        // transform: 'rotate(45deg)',
+        margin: 100,
+    }
 });
 
 class ProfilePicture extends React.Component {
@@ -26,13 +30,20 @@ class ProfilePicture extends React.Component {
         // const { navigate } = this.props.navigation;
         return (
             <View style={{
-                flexDirection:'row',
-                justifyContent:"center"
+                paddingBottom: 20,
+                flexDirection: 'row',
+                justifyContent: "center"
             }}>
+                <View style={styles.main} >
+                    {/* <View style={} ></View>
+                    <View style={} ></View>
+                    <View style={} ></View> */}
+                </View>
+
                 <Image
                     style={{
-                        width:100,
-                        height:100,   
+                        width: 100,
+                        height: 100,
                     }}
                     source={require('../images/profile-picture.jpeg')}
                 />
